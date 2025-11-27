3e2d4f0e307f4d80ff6cca6e3c0a989b0de4982a
mohamed yasser <medoyasser511@gmail.com>
2025-11-27 20:39:33 +0200

    cart list

--- lib/features/checkout/presentation/views/payment_details.dart
+++ lib/features/checkout/presentation/views/payment_details.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:payment_integration/core/widgets/buildAppBar.dart';
+import 'package:payment_integration/features/checkout/presentation/views/widgets/payment_deyails_view_body.dart';
 
 class PaymentDetails extends StatelessWidget {
   const PaymentDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: buildAppBar(title: 'Payment Details'));
+    return Scaffold(appBar: buildAppBar(title: 'Payment Details') , body: PaymentDeyailsViewBody(),);
   }
 }
