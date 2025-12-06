b73706a768d9a4676ccf60e279a1846d41291944
mohamed yasser <medoyasser511@gmail.com>
2025-12-06 15:44:14 +0200

    create repo impl

---
+++ lib/features/checkout/data/repos/checkout_repo_impl.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:payment_integration/core/errors/failure.dart';
+import 'package:payment_integration/core/utils/stripe_service.dart';
+import 'package:payment_integration/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:payment_integration/features/checkout/data/repos/checkout_repo.dart';
+
+class CheckoutRepoImpl extends CheckoutRepo {
+   final StripeService stripeService = StripeService();
+  @override
+  Future<Either<Failure, void>> makepayment({
+    required PaymentIntentInputModel paymentIntentInputModel,
+  }) async {
+    try {
+      await stripeService.makePayment(
+        paymentIntentInputModel: paymentIntentInputModel,
+      );
+      return Right(null);
+    } catch (e) {
+      return Left(ServerError(e.toString()));
+    }
+  }
+}
